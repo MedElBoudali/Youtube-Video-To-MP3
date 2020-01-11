7daa662719aaec69b739186ed2e51ce8fcb2f55b
unknown <mr.elboudali@gmal.com>
2020-01-11 15:32:24 +0000

    dscsdc

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const flash = require("connect-flash");
-const session = require('express-session');
+const session = require('cookie-session');
 const path = require('path');
 const app = express();
 const http = require("http").Server(app);
